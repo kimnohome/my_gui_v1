d5d400262991bd1088cf2e460f1e693119e6908f
kimnohome <287480609@qq.com>
2018-03-08 21:33:42 +0800

    add font class and button font
    加入了文字笔刷，也在button里面加入了文字类，实现按钮上写文字

--- resource/Button.py
+++ resource/Button.py
@@ -14,52 +14,69 @@ from Component import *
 from pygame.locals import *
 from TextManager import *
 from CanvasManager import *
+from TextManager import Font_Brush
+
+
 class Button(Component):
-  def __init__(self, upimage, inimage, downimage,position,size):
-      self.imageUp = pygame.image.load(upimage).convert_alpha()
-      self.imageIn = pygame.image.load(inimage).convert_alpha()
-      self.imageDown = pygame.image.load(downimage).convert_alpha()
+    def __init__(self, upimage, inimage, downimage, position, size):
+        self.imageUp = pygame.image.load(upimage).convert_alpha()
+        self.imageIn = pygame.image.load(inimage).convert_alpha()
+        self.imageDown = pygame.image.load(downimage).convert_alpha()
+        self.imageUp = pygame.transform.scale(self.imageUp, size)
+        self.imageIn = pygame.transform.scale(self.imageIn, size)
+        self.imageDown = pygame.transform.scale(self.imageDown, size)
+        self.positionX, self.positionY = position
+        self.imageWidth, self.imageHeight = self.imageUp.get_size()
+        self.draw_position = (self.positionX - self.imageWidth / 2, self.positionY - self.imageHeight / 2)
+        self.draw_image = self.imageUp
+        self.text_obj = []
 
-      self.imageUp = pygame.transform.scale(self.imageUp, size)
-      self.imageIn = pygame.transform.scale(self.imageIn, size)
-      self.imageDown = pygame.transform.scale(self.imageDown, size)
+    def set_text(self, textObj):
+        self.text_obj = textObj
 
-      self.positionX, self.positionY = position
-      self.imageWidth, self.imageHeight = self.imageUp.get_size()
-      self.draw_position = (self.positionX - self.imageWidth / 2, self.positionY - self.imageHeight / 2)
-      self.draw_image=self.imageUp
+    def set_text(self, text):  # 默认字体
+        self.text_obj = Font_Brush()
+        self.text_obj.setText(text)
+        self.text_obj.font_color = (0, 0, 0)
+        self.text_obj.font_size = 20
+        text_rec = self.text_obj.text_fmt.get_rect()
+        text_rec.center = (self.imageWidth / 2, self.imageHeight / 2)
+        self.text_obj.position = text_rec
 
-  def is_Overlap(self):
-      point_x,point_y = pygame.mouse.get_pos()
-      in_x = self.positionX - self.imageWidth/2 < point_x < self.positionX + self.imageWidth/2
-      in_y = self.positionY - self.imageHeight/2 < point_y < self.positionY + self.imageHeight/2
-      return in_x and in_y
+    def is_Overlap(self):
+        point_x, point_y = pygame.mouse.get_pos()
+        in_x = self.positionX - self.imageWidth / 2 < point_x < self.positionX + self.imageWidth / 2
+        in_y = self.positionY - self.imageHeight / 2 < point_y < self.positionY + self.imageHeight / 2
+        return in_x and in_y
 
-  def mouse_In(self):
-      self.draw_image=self.imageIn
+    def mouse_In(self):
+        self.draw_image = self.imageIn
 
-  def mouse_Down(self):
-      self.draw_image = self.imageDown
+    def mouse_Down(self):
+        self.draw_image = self.imageDown
 
-  def mouse_Up(self):
-      self.draw_image = self.imageUp
+    def mouse_Up(self):
+        self.draw_image = self.imageUp
 
-  def Update(self,screen):
-      screen.blit(self.draw_image, self.draw_position)
+    def Update(self, screen):
+        self.text_obj.Update(self.draw_image)
+        screen.blit(self.draw_image, self.draw_position)
 
-  def onClick(self):
-      pass
+    def onClick(self):
+        pass
+
 
 class ButtonListener(Listener):
     def __init__(self, button):
         self.button = button
-    def onChange(self,event):
+
+    def onChange(self, event):
         if event.type == MOUSEMOTION:
             if self.button.is_Overlap():
                 self.button.mouse_In()
             else:
                 self.button.mouse_Up()
-        elif event.type == MOUSEBUTTONDOWN:#left mouse down
+        elif event.type == MOUSEBUTTONDOWN:  # left mouse down
             if self.button.is_Overlap():
                 self.button.mouse_Down()
                 self.button.onClick()
@@ -68,25 +85,31 @@ class ButtonListener(Listener):
         elif event.type == MOUSEBUTTONUP:
             self.button.mouse_Up()
 
+
 if __name__ == "__main__":
-    #get button images
-    project_file_dir= os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    upImageFilename = project_file_dir+'/assets/button/button1_up.png'
-    downImageFilename = project_file_dir+'/assets/button/button1_down.png'
-    inImageFilename = project_file_dir+'/assets/button/button1_in.png'
+    # get button images
+    project_file_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    upImageFilename = project_file_dir + '/assets/button/button1_up.png'
+    downImageFilename = project_file_dir + '/assets/button/button1_down.png'
+    inImageFilename = project_file_dir + '/assets/button/button1_in.png'
 
     pygame.init()
-    screen = pygame.display.set_mode((400,400),0,32)
+    screen = pygame.display.set_mode((400, 400), 0, 32)
     pygame.display.set_caption("Button test")
+
     class MyButton(Button):
-        def __init__(self, upimage, inimage, downimage,position,size):
-            super(MyButton, self).__init__(upimage, inimage, downimage,position,size)
+        def __init__(self, upimage, inimage, downimage, position, size):
+            super(MyButton, self).__init__(upimage, inimage, downimage, position, size)
             # some special fundtion here
+
         def onClick(self):
             print '按了MyButton一下'
-    button = MyButton(upImageFilename, inImageFilename, downImageFilename, (150, 100),(200,50))
-    myButtonListener=ButtonListener(button)
-    eventManager=EventManager()
+
+    button = MyButton(upImageFilename, inImageFilename,
+                      downImageFilename, (150, 100), (200, 50))
+    button.set_text('test button')
+    myButtonListener = ButtonListener(button)
+    eventManager = EventManager()
     eventManager.addListener(myButtonListener)
     while True:
         event = pygame.event.wait()
@@ -94,7 +117,7 @@ if __name__ == "__main__":
             pygame.quit()
             exit()
         screen.fill((200, 200, 200))
-      #  button.render(screen)
+        #  button.render(screen)
         eventManager.eventManage(event)
         button.Update(screen)
         pygame.display.update()
--- resource/TextManager.py
+++ resource/TextManager.py
@@ -15,53 +15,121 @@
 # 这里似乎应该用装饰器模式把具体font外包出去，而TextManager则只管理font>>>想想还是算了
 # 》》想想还是需要用装饰器，把具体文字内容，位置全部外包出去，然后一个字形的manager就管那个字形的所有文字
 
+#  要避免一个问题就是：先把文字画在一个图上面，图画到屏幕上以后，又再次画一遍文字
 import pygame
 from Component import *
-class TextManager(Component):
-    def __init__(self,font="arial", height=16,anti=True,font_color=(0,0,0),bg_color=(255,255,255)): # 暂时不管自定义ttf字体
+
+# 暂时不要用这个类
+class TextManager(Component):  # no use,后面打算把TextManager弄成和CanvasManager一样
+    def __init__(self, font="arial", height=16, anti=True, font_color=(0, 0, 0),
+                 bg_color=(255, 255, 255)):  # 暂时不管自定义ttf字体
         super(TextManager, self).__init__()
-        self.anti=anti
-        self.font_color=font_color
-        self.bg_color=bg_color
+        self.anti = anti
+        self.font_color = font_color
+        self.bg_color = bg_color
         self.font = pygame.font.SysFont(font, height)
         self.text_surface = self.font.render("hello", anti, font_color, bg_color)
 
-    def Update(self, surface,text,position):
+    def Update(self, surface, text, position):
         if self.bg_color == 'no':
             text_surface = self.font.render(text, self.anti, self.font_color)
         else:
             text_surface = self.font.render(text, self.anti, self.font_color, self.bg_color)
-        textRectObj= text_surface.get_rect()
-        textRectObj.center = (position)
+        textRectObj = text_surface.get_rect()
+        textRectObj.center = position
         surface.blit(text_surface, textRectObj)
+
     def is_Overlap(self):
         pass
 
 
+class Font_Brush(Component):  # used to define font instance
+    def __init__(self, surface_handle):
+        self.__surface_handle = surface_handle
+        self.position = (0, 0)
+        self.text = 'Love U Forever'
+        self.font_color = (255, 0, 0)
+        self.bg_color = 'no'
+        self.font_bold = False  # 是否粗体
+        self.anti = True
+        self.font_italic = False  # 是否斜体
+        self.font_size = 16
+        self.font = pygame.font.SysFont('arial', 16)
+        self.text_fmt = self.font.render(self.text, self.anti, self.font_color)  # no font bg
 
-class Font_No_Use(object):  #no use
     def __init__(self):
-        pass
-    def getText(self):
-        pass
+        self.__surface_handle = []
+        self.position = (0, 0)
+        self.text = 'Love U Forever'
+        self.font_color = (255, 0, 0)
+        self.bg_color = 'no'
+        self.font_bold = False  # 是否粗体
+        self.anti = True
+        self.font_italic = False  # 是否斜体
+        self.font_size = 16
+        self.font = pygame.font.SysFont('arial', 16)
+
+    def setText(self, text):
+        self.text = text
+        if self.bg_color == 'no':
+            self.text_fmt = self.font.render(self.text, self.anti, self.font_color)  # no font bg
+        else:
+            self.text_fmt = self.font.render(self.text, self.anti, self.font_color,
+                                          self.bg_color)  # set font bg
+
+    def setSysFont(self, font, font_size):
+        self.font_size = font_size
+        self.font = pygame.font.SysFont(font, font_size)
+
+    def setFont(self, font, font_size):
+        self.font_size = font_size
+        self.font = pygame.font.Font(font, font_size)
+
+    def setSurface(self, surface):
+        self.__surface_handle = surface
+
+    def font_set_bold(self, font_bold):
+        self.font_bold = font_bold  # 是否粗体
+        self.font.set_bold(font_bold)
+
+    def font_set_italic(self, italic):
+        self.font_italic = italic  # 是否粗体
+        self.font.set_italic(italic)
+
+    def Update(self):
+        # 绘制文字
+        self.__surface_handle.blit(self.text_fmt, self.position)
+
+    def Update(self, surface_handle):
+        if self.bg_color == 'no':
+            text_fmt = self.font.render(self.text, self.anti, self.font_color)  # no font bg
+        else:
+            text_fmt = self.font.render(self.text, self.anti, self.font_color,
+                                          self.bg_color)  # set font bg
+        # 绘制文字
+        surface_handle.blit(text_fmt, self.position)
 
 if __name__ == '__main__':
     import os
     print '测试'
     pygame.init()
-    screen = pygame.display.set_mode((400,400),0,32)
+    screen = pygame.display.set_mode((400, 400), 0, 32)
     pygame.display.set_caption("font test")
-    text_m=TextManager(font_color=(255,0,0),bg_color='no')
+
     project_file_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    image = project_file_dir+'/assets/button/button1_in.png'
+    image = project_file_dir + '/assets/button/button1_in.png'
     imageObj = pygame.image.load(image).convert_alpha()
+    text_1 = Font_Brush(imageObj)
+    text_2 = Font_Brush(screen)
+    text_2.font_color = (0, 255, 0)
+    text_2.setText('Come On')
     while True:
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         screen.fill((200, 200, 200))
-        text_m.Update(imageObj,u'Come On',(20,20))
-        text_m.Update(screen, u'I Love U', (200, 250))
-        screen.blit(imageObj,(100,50))
+        text_1.Update()
+        text_2.Update()
+        screen.blit(imageObj, (100, 50))
         pygame.display.update()
